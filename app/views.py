31940eb574545c491f580fa77c6c3b2797a04cde
jonell-nelson <jonellnelson18uni@gmail.com>
2025-03-14 23:39:07 -0500

    Completed Exercise 2

--- app/views.py
+++ app/views.py
@@ -1,99 +1,136 @@
 import os
-from flask import render_template, request, redirect, url_for, flash, session, abort, Blueprint
+from app import app, db, login_manager
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
-from werkzeug.security import check_password_hash  # Required for password verification
-from app import db, login_manager
+from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
+from app.forms import UploadForm
+from app.utils import get_uploaded_images
 
-# Define Blueprint
-main = Blueprint('main', __name__)
 
-### Routing for your application ###
+###
+# Routing for your application.
+###
 
-@main.route('/')
+@app.route('/')
 def home():
-    return "Hello, Flask!"
+    """Render website's home page."""
+    return render_template('home.html')
 
 
-@main.route('/about/')
+@app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Jonell Nelson")
 
 
-@main.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
-    form = UploadForm()  # Ensure UploadForm is imported
+    form = UploadForm()
 
     if form.validate_on_submit():
-        file = form.file.data  # Assuming the form has a 'file' field
+        # Get file data and save to your uploads folder
+        file = form.photo.data
         filename = secure_filename(file.filename)
-        file.save(os.path.join(Config.UPLOAD_FOLDER, filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         flash('File Saved', 'success')
-        return redirect(url_for('main.upload'))  # Redirect to upload page
+        return redirect(url_for('home'))  # Update this to redirect to a route that displays all uploaded image files
 
     return render_template('upload.html', form=form)
 
+@app.route('/uploads/<filename>')
+@login_required
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
 
-@main.route('/login', methods=['POST', 'GET'])
+@app.route('/files')
+@login_required
+def files():
+    upload_folder = app.config['UPLOAD_FOLDER']
+    image_list = get_uploaded_images(upload_folder)
+    return render_template('files.html', image_list=image_list)
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
 
-    if form.validate_on_submit():  # Proper form validation
+    # change this to actually validate the entire form submission
+    # and not just one field
+    if form.validate_on_submit():
+        # Get the username and password values from the form.
+
         username = form.username.data
         password = form.password.data
 
-        # Query user by username
-        user = db.session.execute(
-            db.select(UserProfile).filter_by(username=username)
-        ).scalar_one_or_none()
+        # Using your model, query database for a user based on the username
+        user = UserProfile.query.filter_by(username=username).first()
 
+        # and password submitted. Remember you need to compare the password hash.
         if user and check_password_hash(user.password, password):
-            login_user(user)
-            flash('Logged in successfully!', 'success')
-            return redirect(url_for("main.upload"))  # Redirect to upload page
 
-        flash('Invalid username or password', 'danger')
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+
+            # Gets user id, load into session
+            login_user(user)
+
+            # Remember to flash a message to the user
+            flash('Login successful!', 'success')
+
+            # The user should be redirected to the upload form instead
+            return redirect(url_for("upload")) 
 
     return render_template("login.html", form=form)
 
+# user_loader callback. This callback is used to reload the user object from
+# the user ID stored in the session
 
-# user_loader callback for Flask-Login
 @login_manager.user_loader
-def load_user(user_id):
-    return db.session.get(UserProfile, user_id)
+def load_user(id):
+    return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('home'))
 
-### General Flask Functions ###
+###
+# The functions below should be applicable to all Flask apps.
+###
 
 # Flash errors from the form if validation fails
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
-            flash(f"Error in the {getattr(form, field).label.text} field - {error}", 'danger')
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+), 'danger')
 
-
-@main.route('/<file_name>.txt')
+@app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
-    return main.send_static_file(file_name + '.txt')
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
 
-
-@main.after_request
+@app.after_request
 def add_header(response):
     """
-    Add headers to force latest IE rendering engine or Chrome Frame
-    and to prevent caching.
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
-
-@main.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
